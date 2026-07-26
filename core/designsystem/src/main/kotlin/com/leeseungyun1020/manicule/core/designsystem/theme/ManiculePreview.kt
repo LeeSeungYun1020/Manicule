4ac6d92c19c1e905eb74ae13aac3e70129f1c27d
Lee Seung Yun <ileilliat@gmail.com>
2026-07-26 17:48:43 +0900

    fix: ManiculePreview 다크모드 배경색 및 정렬 순서 수정

--- core/designsystem/src/main/kotlin/com/leeseungyun1020/manicule/core/designsystem/theme/ManiculePreview.kt
+++ core/designsystem/src/main/kotlin/com/leeseungyun1020/manicule/core/designsystem/theme/ManiculePreview.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FUNCTION)
-@Preview(name = "Light", uiMode = UI_MODE_NIGHT_NO, showBackground = true)
-@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES, showBackground = true)
-@Preview(name = "Font 1.5x", fontScale = 1.5f, showBackground = true)
+@Preview(name = "1. Light", uiMode = UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(name = "2. Dark", uiMode = UI_MODE_NIGHT_YES, showBackground = true, backgroundColor = 0xFF1A1410)
+@Preview(name = "3. Font 1.5x", fontScale = 1.5f, showBackground = true)
 annotation class ManiculePreview
