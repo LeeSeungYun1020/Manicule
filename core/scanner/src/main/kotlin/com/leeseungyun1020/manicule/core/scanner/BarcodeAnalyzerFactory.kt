01e7ca60f0e7882a17766a69900ea16210d45a3e
Lee Seung Yun <ileilliat@gmail.com>
2026-08-14 16:06:08 +0900

    refactor: 기존 바코드 분석 계약 제거

--- core/scanner/src/main/kotlin/com/leeseungyun1020/manicule/core/scanner/BarcodeAnalyzerFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.leeseungyun1020.manicule.core.scanner
-
-import java.util.concurrent.Executor
-
-fun interface BarcodeAnalyzerFactory {
-    fun create(executor: Executor): BarcodeAnalyzerSession
-}
--- core/scanner/src/main/kotlin/com/leeseungyun1020/manicule/core/scanner/BarcodeAnalyzerSession.kt
+++
@@ -1,9 +0,0 @@
-package com.leeseungyun1020.manicule.core.scanner
-
-import androidx.camera.core.ImageAnalysis
-
-interface BarcodeAnalyzerSession : AutoCloseable {
-    val analyzer: ImageAnalysis.Analyzer
-
-    suspend fun getBarcodes(predicate: (String) -> Boolean = { true }): List<String>
-}
